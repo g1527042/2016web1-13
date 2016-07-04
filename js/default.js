67a8afe470bd5c401855eb20ee7dbceb5ed5edfd
g1527042 <g1527042@st.t-kougei.ac.jp>
2016-07-04 17:27:59 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,10 @@
 var separate_time = function(time){
- var sec = time.getSeconds();
- var min = time.getMinutes();
+ var sec   = time.getSeconds();
+ var min   = time.getMinutes();
  var hours = time.getHours();
- var days = time.getDate();
+ var days  = time.getDate();
  var month = time.getMonth();
- var year = time.getFullYear();
+ var year  = time.getFullYear();
  return [sec, min, hours, days, month, year];
 }
 
