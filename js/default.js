293260aad093884ea12f2af5a494091512cb325d
g1527042 <g1527042@st.t-kougei.ac.jp>
2016-07-04 18:37:55 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -47,6 +47,11 @@ if(last_date){
 var current_time = new Date();
 setCookie('lastDate', current_time.toString(), 7);
 
+// クッキーの削除
+document.getElementById('remove_cookie').onsubmit = function(){
+ setCookie('lastDate', "", 0);
+};
+
 function getFileName() {
  // ホスト以下のパスを入力し、それをscript('/')で「/」区切りで配列に入れ、pop()で配列の最後の値を取得
  return window.location.href.split('/').pop();
