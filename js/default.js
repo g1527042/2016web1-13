b17b7a90143d387b0ad4111cfc92070c3883396c
g1527042 <g1527042@st.t-kougei.ac.jp>
2016-07-04 17:53:09 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -11,6 +11,10 @@ var update = function(){
   var target = new Date(2020, 7, 24, 0, 0, 0, 0);
   var diff = target.getTime() - now.getTime();
   var counter = separate_time(diff);
+  
+  document.getElementById('form').select.onchange = function(){
+   location.href = document.getElementById('form').select.value;
+  }
   document.getElementById('countdown').textContent = 
     '東京オリンピックまであと　' +
     counter[3] + '日' +
